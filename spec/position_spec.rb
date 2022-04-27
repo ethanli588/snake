239cdd805d001c2eb13988d2f67a6fb1760fb58e
Ethan Li <ethanli588@gmail.com>
2022-04-27 21:34:11 +1000

    add some test cases

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -4,8 +4,18 @@ describe Position do
   let(:position) { Position.new(12, 12) }
 
   describe '#move_towards' do
-    it 'Move LEFT to right place' do
-      expect(position.move_towards(Direction.new(Direction::LEFT), 12)).to eq Position.new(0, 12)
+    it 'Move towards LEFT' do
+      expect(position.move_towards(Direction.new(Direction::LEFT), 12)).to eq(Position.new(0, 12))
+    end
+  end
+
+  describe "#copy!" do
+    it "Copy from a position instance" do
+      position.copy!(Position.new(10, 10))
+      actual = position
+      expected = Position.new(10, 10)
+
+      expect(actual).to eq(expected)
     end
   end
 end
